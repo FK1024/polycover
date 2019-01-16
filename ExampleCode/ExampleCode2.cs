fe59d537bd0732dc69dbafd0502930147c42772a
Felix Keck <felixkeck96@gmx.de>
2019-01-16 13:23:39 +0100

    added tests for abstract classes and methods

---
+++ ExampleCode/ExampleCode2.cs
@@ -0,0 +1,45 @@
+﻿namespace ExampleCode2
+{
+    /*
+        Class Structure:
+
+            BaseClass
+                ^
+                |
+            *DerivedAbstractClass*
+                ^
+                |
+            DerivedConcreteClass
+    */
+
+    public class BaseClass
+    {
+        public virtual int Method1(int a)
+        {
+            return a;
+        }
+    }
+
+    public abstract class DerivedAbstractClass : BaseClass
+    {
+        public override int Method1(int a)
+        {
+            return a + 1;
+        }
+
+        public abstract double Method2();
+    }
+
+    public class DerivedConcreteClass : DerivedAbstractClass
+    {
+        public override double Method2()
+        {
+            return 0;
+        }
+
+        public bool Method3(bool b)
+        {
+            return b;
+        }
+    }
+}
---
+++ StaticCodeAnalysisTests/StaticCodeAnalysisTests2.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StaticCodeAnalysis;
+using System;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace StaticCodeAnalysis.Tests
+{
+    [TestClass()]
+    public class StaticCodeAnalysisTests2
+    {
+        static string path = @"..\..\..\ExampleCode\ExampleCode2.cs";
+        StaticCodeAnalysis testAnalysis = new StaticCodeAnalysis(path);
+
+        // getBaseClasses tests:
+        // =====================
+        [TestMethod()]
+        public void getBaseClassesTest1()
+        {
+            var expected = new List<INamedTypeSymbol> { };
+            var actual = testAnalysis.getBaseClasses(testAnalysis.getClassDeclSyntax("ExampleCode2.BaseClass"));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void getBaseClassesTest2()
+        {
+            var expected = new List<INamedTypeSymbol>
+            {
+                testAnalysis.GetClassNamedTypeSymbol("ExampleCode2.BaseClass")
+            };
+            var actual = testAnalysis.getBaseClasses(testAnalysis.getClassDeclSyntax("ExampleCode2.DerivedAbstractClass"));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void getBaseClassesTest3()
+        {
+            var expected = new List<INamedTypeSymbol>
+            {
+                testAnalysis.GetClassNamedTypeSymbol("ExampleCode2.DerivedAbstractClass"),
+                testAnalysis.GetClassNamedTypeSymbol("ExampleCode2.BaseClass")
+            };
+            var actual = testAnalysis.getBaseClasses(testAnalysis.getClassDeclSyntax("ExampleCode2.DerivedConcreteClass"));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        // getDerivedClasses tests:
+        // ========================
+        [TestMethod()]
+        public void getDerivedClassesTest1()
+        {
+            var expected = new List<INamedTypeSymbol>
+            {
+                testAnalysis.GetClassNamedTypeSymbol("ExampleCode2.DerivedAbstractClass"),
+                testAnalysis.GetClassNamedTypeSymbol("ExampleCode2.DerivedConcreteClass")
+            };
+            var actual = testAnalysis.getDerivedClasses(testAnalysis.getClassDeclSyntax("ExampleCode2.BaseClass"));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void getDerivedClassesTest2()
+        {
+            var expected = new List<INamedTypeSymbol>
+            {
+                testAnalysis.GetClassNamedTypeSymbol("ExampleCode2.DerivedConcreteClass")
+            };
+            var actual = testAnalysis.getDerivedClasses(testAnalysis.getClassDeclSyntax("ExampleCode2.DerivedAbstractClass"));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void getDerivedClassesTest3()
+        {
+            var expected = new List<INamedTypeSymbol> { };
+            var actual = testAnalysis.getDerivedClasses(testAnalysis.getClassDeclSyntax("ExampleCode2.DerivedConcreteClass"));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        // getMethods tests:
+        // =================
+        [TestMethod()]
+        public void getMethodsTest1()
+        {
+            var expected = new List<MethodDeclarationSyntax>
+            {
+                testAnalysis.getMethodDeclSyntax("ExampleCode2.BaseClass.Method1")
+            };
+            var actual = testAnalysis.getMethods(testAnalysis.getClassDeclSyntax("ExampleCode2.BaseClass"));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void getMethodsTest2()
+        {
+            var expected = new List<MethodDeclarationSyntax>
+            {
+                testAnalysis.getMethodDeclSyntax("ExampleCode2.DerivedAbstractClass.Method1"),
+                testAnalysis.getMethodDeclSyntax("ExampleCode2.DerivedAbstractClass.Method2")
+            };
+            var actual = testAnalysis.getMethods(testAnalysis.getClassDeclSyntax("ExampleCode2.DerivedAbstractClass"));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void getMethodsTest3()
+        {
+            var expected = new List<MethodDeclarationSyntax>
+            {
+                testAnalysis.getMethodDeclSyntax("ExampleCode2.DerivedConcreteClass.Method2"),
+                testAnalysis.getMethodDeclSyntax("ExampleCode2.DerivedConcreteClass.Method3")
+            };
+            var actual = testAnalysis.getMethods(testAnalysis.getClassDeclSyntax("ExampleCode2.DerivedConcreteClass"));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        
+        // getOverridingMethods tests:
+        // ===========================
+        [TestMethod()]
+        public void getOverridingMethodsTest1()
+        {
+            var expected = new List<MethodDeclarationSyntax>
+            {
+                testAnalysis.getMethodDeclSyntax("ExampleCode2.DerivedAbstractClass.Method1")
+            };
+            var actual = testAnalysis.getOverridingMethods(testAnalysis.GetMethodNamedTypeSymbol("ExampleCode2.BaseClass.Method1"));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void getOverridingMethodsTest2()
+        {
+            var expected = new List<MethodDeclarationSyntax> { };
+            var actual = testAnalysis.getOverridingMethods(testAnalysis.GetMethodNamedTypeSymbol("ExampleCode2.DerivedAbstractClass.Method1"));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void getOverridingMethodsTest3()
+        {
+            var expected = new List<MethodDeclarationSyntax>
+            {
+                testAnalysis.getMethodDeclSyntax("ExampleCode2.DerivedConcreteClass.Method2")
+            };
+            var actual = testAnalysis.getOverridingMethods(testAnalysis.GetMethodNamedTypeSymbol("ExampleCode2.DerivedAbstractClass.Method2"));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void getOverridingMethodsTest4()
+        {
+            var expected = new List<MethodDeclarationSyntax> { };
+            var actual = testAnalysis.getOverridingMethods(testAnalysis.GetMethodNamedTypeSymbol("ExampleCode2.DerivedConcreteClass.Method2"));
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
