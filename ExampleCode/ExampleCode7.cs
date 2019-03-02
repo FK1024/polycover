f47d33ed5a1feb562022141bd3319fd71ca8f34a
Felix Keck <felixkeck96@gmx.de>
2019-03-02 14:01:37 +0100

    two more example code files

---
+++ ExampleCode/ExampleCode7.cs
@@ -0,0 +1,59 @@
+﻿namespace ExampleCode7
+{
+    /*
+        Class Structure:
+
+            Prog
+
+            C1
+            ^
+            |
+            C2
+            ^
+            |
+            C3
+    */
+
+    public class Prog
+    {
+        public int N(int c, int s)
+        {
+            switch (c)
+            {
+                case 1: return new C1().M(c, s);
+                case 2: return new C2().M(c, s);
+                case 3: return new C3().M(c, s);
+                default: return s;
+            }
+        }
+
+        public int MyInc(int c, int s)
+        {
+            return N(c + 1, s);
+        }
+    }
+    
+    public class C1
+    {
+        public virtual int M(int c, int s)
+        {
+            return new Prog().MyInc(c, s + 1);
+        }
+    }
+
+    public class C2 : C1
+    {
+        public override int M(int c, int s)
+        {
+            return new Prog().MyInc(c, s + 2);
+        }
+    }
+
+    public class C3 : C2
+    {
+        public override int M(int c, int s)
+        {
+            return new Prog().MyInc(c, s + 3);
+        }
+    }
+}
---
+++ ExampleCode/ExampleCode8.cs
@@ -0,0 +1,85 @@
+﻿namespace ExampleCode8
+{
+    /*
+        Class Structure:
+
+            <<interface>>
+                   I1
+                   ^^
+                  /  \
+                 C1   C3
+                 ^
+                 |
+                 C2
+    */
+
+    public interface I1
+    {
+        int M2(I1 c);
+        int M3(I1 c);
+    }
+
+    public class C1 : I1
+    {
+        public int M1(I1 c)
+        {
+            C3 c3 = new C3();
+            c3.M2(c);
+            M4(c);
+            return M2(c);
+        }
+
+        public int M2(I1 c)
+        {
+            C2 c2 = new C2();
+            c2.M3(c);
+            M5(c);
+            return c.M3(c);
+        }
+
+        public virtual int M3(I1 c)
+        {
+            return M5(c);
+        }
+
+        public int M4(I1 c)
+        {
+            M5(c);
+            return M2(c);
+        }
+
+        public int M5(I1 c)
+        {
+            return M1(c);
+        }
+
+        public int M6()
+        {
+            return M3(new C2());
+        }
+    }
+
+    public class C2 : C1
+    {
+        public override int M3(I1 c)
+        {
+            return 23;
+        }
+    }
+
+    public class C3 : I1
+    {
+        public int M2(I1 c)
+        {
+            C2 c2 = new C2();
+            c2.M3(c);
+            return c.M3(c);
+        }
+
+        public int M3(I1 c)
+        {
+            C1 c1 = new C1();
+            return c1.M1(c);
+        }
+    }
+}
