983a5676b380a3ec46ad7b4b91020cadfcdf1c29
Felix Keck <felixkeck96@gmx.de>
2019-03-02 14:00:58 +0100

    Added coverage results for ExampleCode6

---
+++ ExampleCodeTests/ExampleCode6Tests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExampleCode6.Tests
+{
+    [TestClass()]
+    public class ATests
+    {
+        [TestMethod()]
+        public void NTest()
+        {
+            Assert.AreEqual(2, new A().N(true));
+        }
+    }
+}
