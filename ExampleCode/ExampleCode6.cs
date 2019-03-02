cf815f916ee8fdf1d236c3db900dfa54ce3e23b8
Felix Keck <felixkeck96@gmx.de>
2019-03-02 13:40:43 +0100

    Added ExampleCode6 + graph + test

---
+++ ExampleCode/ExampleCode6.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace ExampleCode6
+{
+    /*
+        Class Structure:
+
+            A
+            ^
+            |
+            B
+    */
+
+    public class A
+    {
+        public int N(bool b)
+        {
+            if (b)
+            {
+                return new B().M();
+            }
+            else
+            {
+                return M();
+            }
+        }
+
+        public virtual int M()
+        {
+            return 1;
+        }
+    }
+
+    public class B : A
+    {
+        public override int M()
+        {
+            StackFrame callerStackFrame = new StackFrame(1, true);
+            string callerFileName = callerStackFrame.GetFileName();
+            int callerLineNumber = callerStackFrame.GetFileLineNumber();
+            string thisFileName = Path.GetFullPath(@"..\..\..\ExampleCode\ExampleCode6.cs");
+            
+            if (callerFileName == thisFileName && callerLineNumber == 21)
+            {
+                
+            }
+            if (callerFileName == thisFileName && callerLineNumber == 25)
+            {
+                
+            }
+
+            return 2;
+        }
+    }
+}
