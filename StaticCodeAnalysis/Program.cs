dd599ad70a60a1afbdcc032dc4096bc60e281fd9
Felix Keck <felixkeck96@gmx.de>
2019-03-18 11:17:42 +0100

    updated Program

--- StaticCodeAnalysis/Program.cs
+++ StaticCodeAnalysis/Program.cs
@@ -1,52 +1,246 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Text;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace StaticCodeAnalysis
 {
     public class Program
     {
-        static void Main(string[] args)
+        private static string backupDirectory;
+
+        private static void DisplayHelp()
         {
-            string codePath = @"..\..\..\ExampleCode\ExampleCode6.cs";
-            StaticCodeAnalysis testAnalysis = new StaticCodeAnalysis(codePath);
+            Console.WriteLine(".NET Framework source code edge coverage tool" + Environment.NewLine);
+
+            Console.WriteLine("Arguments:");
+            Console.WriteLine("\t" + "<CODEFILE>" + "\t" + "Path to the source code file");
+            Console.WriteLine("\t" + "<SOLUTION>" + "\t" + "Path to the solution");
+            Console.WriteLine("\t" + "<TESTPROJECT>" + "\t" + "Path to the test project");
+            Console.WriteLine("\t" + "<TESTASSEMBLY>" + "\t" + "Path to the test assembly");
+        }
+
+        private static void ParseArguments(string[] args)
+        {
+            if (args.Length != 4)
+            {
+                throw new ArgumentException("Wrong number of arguments specified");
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (!File.Exists(args[i]))
+                {
+                    throw new ArgumentException(new string[] { "Code", "Solution", "Project", "Assembly" }[i] + $" file does not exist: '{args[i]}'");
+                }
+            }
+        }
+        
+
+        static int Main(string[] args)
+        {
+            if (args.Length == 1 && (args[0] == "-h" || args[0] == "--help" || args[0] == "/?"))
+            {
+                DisplayHelp();
+                return 0;
+            }
+            else
+            {
+                try
+                {
+                    ParseArguments(args);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    DisplayHelp();
+                    return 1;
+                }
+            }
+
+            backupDirectory = FindBackupDirectory();
+
+            // input parameters:
+            string codePath = args[0];
+            string solPath = args[1];
+            string testProjPath = args[2];
+            string testDllPath = args[3];
+
+            string target = "--target dotnet";
+            string targetargs = $"--targetargs \"test {testProjPath} --no-build\"";
+            string coveragePath = backupDirectory + @"\coverage.json";
+            string output = $"--output {coveragePath}";
+            string coverletCall = String.Join(" ", new string[] { "coverlet", testDllPath, target, targetargs, output });
+
+            string buildCall = String.Join(" ", new string[] { "dotnet", "build", solPath });
+
+
+            StaticCodeAnalysis codeAnalysis = new StaticCodeAnalysis(codePath);
+
+            YoYoGraph graph = CreateYoYoGraph(codeAnalysis);
+
+            BackupFile(codePath);
+
+            try
+            {
+                // manipulate the source code file to be able to gather information needed
+                PreCorrectLineNumbers(graph);
+                SyntaxRewriter rewriter = new SyntaxRewriter(codeAnalysis, graph);
+                SyntaxNode newRoot = rewriter.Visit(codeAnalysis.GetRoot());
+                File.WriteAllText(codePath, newRoot.GetText().ToString(), Encoding.Default);
+                Console.WriteLine(Environment.NewLine + $"Instrumented file '{codePath}'");
+
+                // re-build the project
+                Process buildProcess = new Process();
+                buildProcess.StartInfo.FileName = "cmd.exe";
+                buildProcess.StartInfo.Arguments = "/C " + buildCall;
+                buildProcess.StartInfo.RedirectStandardOutput = true; // don't write stdout to console
+                buildProcess.StartInfo.UseShellExecute = false; // don't create new window
+                Console.WriteLine(Environment.NewLine + "Re-building project ...");
+                buildProcess.Start();
+                buildProcess.WaitForExit();
+                if (buildProcess.ExitCode != 0)
+                {
+                    Console.WriteLine("build failed");
+                    return 1;
+                }
+                Console.WriteLine("build succeed");
+
+                // start coverlet
+                Process coverletProcess = new Process();
+                coverletProcess.StartInfo.FileName = "cmd.exe";
+                coverletProcess.StartInfo.Arguments = "/C " + coverletCall;
+                coverletProcess.StartInfo.RedirectStandardOutput = true; // don't write stdout to console
+                coverletProcess.StartInfo.UseShellExecute = false; // don't create new window
+                Console.WriteLine(Environment.NewLine + "Starting coverlet ...");
+                coverletProcess.Start();
+                coverletProcess.WaitForExit();
+                if (coverletProcess.ExitCode != 0)
+                {
+                    Console.WriteLine("coverlet calculation failed");
+                    return 1;
+                }
+                Console.WriteLine("coverlet calculation succeed");
+
+                // read the generated json file & calculate the coverage
+                Console.WriteLine(Environment.NewLine + "Calculating coverage result ...");
+                Coverage coverage = new Coverage(coveragePath, codePath, graph);
+                double result = coverage.Calculate();
+
+                // create dgml file
+                string graphPath = codePath.Substring(0, codePath.Length - 3) + "YoYoGraph.dgml";
+                graph.Serialize(graphPath);
+                Console.WriteLine($"Generated YoYo-graph '{graphPath}'");
+
+                Console.WriteLine(Environment.NewLine + "OO Edge Pair Coverage Result: {0:P2}" + Environment.NewLine, result);
+            }
+            finally
+            {
+                RestoreFile(codePath);
+            }
             
-            YoYoGraph testGraph = new YoYoGraph();
+            return 0;
+        }
+
+
+        // creates and returns the YoYo graph
+        public static YoYoGraph CreateYoYoGraph(StaticCodeAnalysis codeAnalysis)
+        {
+            YoYoGraph graph = new YoYoGraph();
             
             // create a node for each method
-            List<MethodDeclarationSyntax> methods = testAnalysis.GetAllMethods();
+            List<MethodDeclarationSyntax> methods = codeAnalysis.GetAllMethods();
 
             foreach (MethodDeclarationSyntax method in methods)
             {
-                string nodeName = testAnalysis.GetFullMethodName(method);
-                testGraph.AddNode(new Node(nodeName, nodeName, method));
+                string nodeName = codeAnalysis.GetFullMethodName(method);
+                graph.AddNode(new Node(nodeName, nodeName, method));
             }
 
             // create a node for each invocation and add the links
-            List<Node> methodNodes = new List<Node>(testGraph.Nodes);
+            List<Node> methodNodes = new List<Node>(graph.Nodes);
             foreach (Node methodNode in methodNodes)
             {
                 int invocCount = 0;
-                List<StaticCodeAnalysis.Invocation> invocations = testAnalysis.GetInvocations(methodNode.Method);
+                List<StaticCodeAnalysis.Invocation> invocations = codeAnalysis.GetInvocations(methodNode.Method);
                 foreach (StaticCodeAnalysis.Invocation invocation in invocations)
                 {
                     Node invocNode = new Node(methodNode.Id + "_I" + invocCount.ToString(), "invocs", invocation);
-                    testGraph.AddNode(invocNode);
+                    graph.AddNode(invocNode);
                     invocCount++;
-                    testGraph.AddLink(new Link(methodNode.Id, invocNode.Id));
+                    graph.AddLink(new Link(methodNode.Id, invocNode.Id));
 
                     foreach (MethodDeclarationSyntax invocOption in invocation.Methods)
                     {
-                        testGraph.AddLink(new Link(invocNode.Id, testGraph.GetNode(testAnalysis.GetFullMethodName(invocOption)).Id));
+                        graph.AddLink(new Link(invocNode.Id, graph.GetNode(codeAnalysis.GetFullMethodName(invocOption)).Id));
                     }
                 }
             }
 
-            string DGMLPath = codePath.Substring(0, codePath.Length - 3) + "YoYoGraph.dgml";
-            testGraph.Serialize(DGMLPath);
+            return graph;
+        }
+
+        // corrects all saved line numbers of invocations with respect to the code which will be insterted
+        public static void PreCorrectLineNumbers(YoYoGraph graph)
+        {
+            // first create a dictionary ["method body start line", "number of lines to insert"]
+            var LinesToInsertAtLine = new Dictionary<int, int>();
+
+            foreach (Node methodNode in graph.GetMethodNodes())
+            {
+                int methodBodyStartLine = methodNode.Method.Body.Statements.First().GetLocation().GetLineSpan().StartLinePosition.Line;
+                int NoIncomingInvocs = graph.GetIncomingLinks(methodNode.Id).Count;
+                int NoLinesToInsert = NoIncomingInvocs > 0 ? 1 + 2 * NoIncomingInvocs : 0;
+                LinesToInsertAtLine.Add(methodBodyStartLine, NoLinesToInsert);
+            }
+
+            // correct all saved line numbers by adding the number of lines getting inserted before
+            foreach (Node invocNode in graph.GetInvocationNodes())
+            {
+                // check if invocation has line numbers which need to be corrected
+                if (invocNode.Invocation.Lines.Any(l => l >= LinesToInsertAtLine.Where(elem => elem.Value > 0).Select(elem => elem.Key).Min()))
+                {
+                    List<int> correctedLines = new List<int>();
+                    foreach (int invocLine in invocNode.Invocation.Lines)
+                    {
+                        int linesToInsert = (from methodLine in LinesToInsertAtLine
+                                                         where methodLine.Key <= invocLine
+                                                         select methodLine.Value).Sum();
+                        correctedLines.Add(invocLine + linesToInsert);
+                    }
+                    invocNode.Invocation = new StaticCodeAnalysis.Invocation(correctedLines, invocNode.Invocation.Methods);
+                }
+            }
+        }
+
+        // searches for a random folder name in the user's temporary folder which not yet exists to safely backup
+        public static string FindBackupDirectory()
+        {
+            string backupDir;
+
+            do
+            {
+                backupDir = Path.Combine(Path.GetTempPath(), Path.GetFileNameWithoutExtension(Path.GetRandomFileName()));
+            } while (Directory.Exists(backupDir));
+
+            return backupDir;
+        }
+        
+        // creates the previously determined backup folder and copies the given file into it
+        public static void BackupFile(string file)
+        {
+            Directory.CreateDirectory(backupDirectory);
+            File.Copy(file, Path.Combine(backupDirectory, Path.GetFileName(file)));
+        }
+        
+        // restores backuped file and deletes the backup folder
+        public static void RestoreFile(string file)
+        {
+            File.Copy(Path.Combine(backupDirectory, Path.GetFileName(file)), file, true);
+            Directory.Delete(backupDirectory, true);
         }
     }
 }
