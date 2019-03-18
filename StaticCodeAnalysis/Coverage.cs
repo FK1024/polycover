b790dd2f760b6b77dfd97cf2dbfabe6c7005162b
Felix Keck <felixkeck96@gmx.de>
2019-03-18 11:15:18 +0100

    added classes SyntaxRewriter and Coverage

---
+++ StaticCodeAnalysis/Coverage.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace StaticCodeAnalysis
+{
+    class Coverage
+    {
+        private YoYoGraph graph;
+        private JToken fileCov;
+
+
+        public Coverage(string jsonPath, string codePath, YoYoGraph graph)
+        {
+            this.graph = graph;
+            JToken coverage = JObject.Parse(File.ReadAllText(jsonPath));
+            JToken projectCov = coverage.Values().First();
+            this.fileCov = projectCov[codePath];
+        }
+
+
+        // calculates coverage results by comparing information from the json file with information from the graph, modifies the graph
+        public double Calculate()
+        {
+            JToken lines = GetLines();
+            int linksCovered = 0;
+
+            // mark the covered links from an invocation node to a method node
+            List<Link> linksFromInvoc2Method = graph.GetLinksFromInvoc2Method();
+            foreach (Link link in linksFromInvoc2Method)
+            {
+                if (lines[(link.TargetInsertedIfBodyLineNumber + 1).ToString()].ToObject<int>() > 0)
+                {
+                    link.IsCovered = true;
+                    linksCovered++;
+                }
+                else
+                {
+                    link.IsCovered = false;
+                }
+            }
+
+            // mark the completely covered invocation nodes and their incoming links
+            List<Node> invocNodes = graph.GetInvocationNodes();
+            foreach (Node invocNode in invocNodes)
+            {
+                List<Link> invocOutgoingLinks = graph.GetOutgoingLinks(invocNode.Id);
+                Link invocIncomingLink = graph.GetIncomingLinks(invocNode.Id).First();
+                // if all outgoing links of an invocation node are covered then the invocation node and it's incoming link are covered too
+                if (invocOutgoingLinks.All(l => l.IsCovered))
+                {
+                    invocNode.IsCovered = true;
+                    invocIncomingLink.IsCovered = true;
+                }
+                else
+                {
+                    invocNode.IsCovered = false;
+                    invocIncomingLink.IsCovered = false;
+                }
+            }
+
+            // mark the completely covered method nodes
+            List<Node> methodNodes = graph.GetMethodNodes();
+            foreach (Node methodNode in methodNodes)
+            {
+                List<Link> methodOutgoingLinks = graph.GetOutgoingLinks(methodNode.Id);
+                // if all outgoing links of a method node are covered then the method node is covered too
+                if (methodOutgoingLinks.All(l => l.IsCovered))
+                {
+                    methodNode.IsCovered = true;
+                }
+                else
+                {
+                    methodNode.IsCovered = false;
+                }
+            }
+
+            double result = (double)linksCovered / linksFromInvoc2Method.Count;
+            return result;
+        }
+
+        // appends all "lines" tokens in current file
+        private JToken GetLines()
+        {
+            var lines = new JObject();
+
+            foreach (JToken classCov in this.fileCov.Values())
+            {
+                foreach (JToken methodCov in classCov.Values())
+                {
+                    lines.Merge(methodCov["Lines"] as JObject);
+                }
+            }
+
+            return lines;
+        }
+    }
+}
---
+++ StaticCodeAnalysis/SyntaxRewriter.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StaticCodeAnalysis
+{
+    class SyntaxRewriter : CSharpSyntaxRewriter
+    {
+        private YoYoGraph graph;
+        private StaticCodeAnalysis codeAnalysis;
+        private int insertedLines = 0;
+
+
+        public SyntaxRewriter(StaticCodeAnalysis codeAnalysis, YoYoGraph graph)
+        {
+            this.codeAnalysis = codeAnalysis;
+            this.graph = graph;
+        }
+
+
+        // inserts an if-statement for each incoming invocation for each method to detect from which line each method was invoced, modifies the YoYo graph
+        public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node) // hopefully this visits the nodes in the correct order
+        {
+            List<Link> incomingInvocations = graph.GetIncomingLinks(codeAnalysis.GetFullMethodName(node));
+            int NoIncomingInvocs = incomingInvocations.Count();
+            if (NoIncomingInvocs == 0)
+            {
+                return node;
+            }
+            else
+            {
+                int methodBodyStartLine = node.Body.Statements.First().GetLocation().GetLineSpan().StartLinePosition.Line + insertedLines;
+                int linesToInsert = 1 + 2 * NoIncomingInvocs;
+                insertedLines += linesToInsert;
+                
+                // first create statement for the assignment of a variable for the caller's line number
+                List<StatementSyntax> ifStatements = new List<StatementSyntax>();
+                string lineNumberAssignmentStr = "int ln = new System.Diagnostics.StackFrame(1, true).GetFileLineNumber() - 1;" + Environment.NewLine; // Note: "- 1" because GetFileLineNumber is 1 based whereas GetLocation().GetLineSpan().StartLinePosition.Line is zero based!
+                ifStatements.Add(SyntaxFactory.ParseStatement(lineNumberAssignmentStr));
+
+                // then create an if-statement for each invocation
+                int linkNo = 0;
+                foreach (Link link in incomingInvocations)
+                {
+                    string ifStatementStr = "if (";
+                    foreach (int line in graph.GetNode(link.Source).Invocation.Lines)
+                    {
+                        ifStatementStr += "ln == " + line + " || ";
+                    }
+                    // remove last " || "
+                    ifStatementStr = ifStatementStr.Substring(0, ifStatementStr.Length - 4);
+                    ifStatementStr += ")" + Environment.NewLine + "{}" + Environment.NewLine;
+
+                    // add the parsed statement
+                    ifStatements.Add(SyntaxFactory.ParseStatement(ifStatementStr));
+
+                    // save the line number where the body of the created statement will be inserted
+                    link.TargetInsertedIfBodyLineNumber = methodBodyStartLine + 2 + 2 * linkNo;
+                    linkNo++;
+                }
+
+                // insert the generated if-statements
+                var newStatements = node.Body.Statements.InsertRange(0, ifStatements);
+                var newBody = node.Body.WithStatements(newStatements);
+                var newMethod = node.WithBody(newBody);
+                return newMethod;
+            }
+        }
+    }
+}
